ff010c5c463cfc4e737079fbdb5af16789ecaf3c
Grigory Sapogov <grisha.sapgv@mail.ru>
2024-03-11 22:25:55 +0500

    Initial Commit

---
+++ LanguageApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LanguageApp
+//
+//  Created by Grigory Sapogov on 11.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
